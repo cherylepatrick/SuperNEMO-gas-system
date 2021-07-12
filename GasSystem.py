42ef7ec7109db8a494265aa446f946a1a78c5719
Cheryl Patrick <c.patrick@ucl.ac.uk>
2021-07-12 15:15:27 +0100

    Now uses a list of files

--- GasSystem.py
+++ GasSystem.py
@@ -7,17 +7,19 @@ import csv # To read the input files
 from datetime import datetime # To parse the dates and times
 
 # Read CSV file
-def parse_file(filename,dict):
-  with open(filename) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count = 0
-    for row in csv_reader:
-      # Parse the date to the nearest microsecond
-      # looks like it's always GMT so don't worry about that
-      timestamp = datetime.strptime(row[0][0:24],'%m/%d/%y %H:%M:%S.%f')
-      dict[timestamp]=row[1] #Hopefully this deals with deduplicating
-      line_count += 1
-    print(f'{filename} contained {line_count} lines.')
+def parse_files(filenames):
+  dict={}
+  for filename in filenames:
+    with open(filename) as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=';')
+      line_count = 0
+      for row in csv_reader:
+        # Parse the date to the nearest microsecond
+        # looks like it's always GMT so don't worry about that
+        timestamp = datetime.strptime(row[0][0:24],'%m/%d/%y %H:%M:%S.%f')
+        dict[timestamp]=row[1] #Hopefully this deals with deduplicating
+        line_count += 1
+      print(f'{filename} contained {line_count} lines.')
   return dict
 
 #Set default figure size
@@ -26,14 +28,14 @@ mpl.rcParams["legend.frameon"] = False
 mpl.rcParams['figure.dpi']=200 # dots per inch
 
 
-dict={} # Create an empty dictionary
-parse_file('data/Up_Pressure_Empty_1.txt',dict)
-
+ # Read all CSV files to a dictionary of timestamp vs. value
+ # That should deduplicate, so the individual totals don't necessarily add up to the final total
+input_files=['data/Up_Pressure_Empty_1.txt','data/Up_Pressure_Empty_2.txt','data/Up_Pressure_Filled_1.txt','data/Up_Pressure_Filled_2.txt']
+dict=parse_files(input_files)
 
 print(f'Dictionary contains {len(dict)} entries')
 
 lists = sorted(dict.items()) # sorted by key, return a list of tuples
-
 timestamps, values = zip(*lists) # unpack a list of pairs into two tuples
 fig, ax = plt.subplots()
 ax.plot(timestamps, values)
