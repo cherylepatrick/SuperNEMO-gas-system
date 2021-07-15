ebdb7c50602b65aa20c1a872f82f55a4b516caef
Cheryl Patrick <c.patrick@ucl.ac.uk>
2021-07-15 17:46:43 +0100

    Rename value to pressure cos it's confusing

--- GasSystem.py
+++ GasSystem.py
@@ -39,48 +39,48 @@ def plot(dict, output_filename):
   Takes a dictionary of timestamps vs pressures and plots it
   """
   lists = sorted(dict.items()) # sorted by key, return a list of tuples
-  timestamps, values = zip(*lists) # unpack a list of pairs into two tuples
+  timestamps, pressures = zip(*lists) # unpack a list of pairs into two tuples
   fig, ax = plt.subplots()
-  ax.plot(timestamps, values)
+  ax.plot(timestamps, pressures)
   ax.set_xlabel("Timestamp")
   ax.set_ylabel("Pressure")
   plt.savefig(output_filename+".png")
 
 
-def is_slope_start (values, pos):
+def is_slope_start (pressures, pos):
   """
-  Is this position in list of values the first in an increasing slope?
+  Is this position in list of pressures the first in an increasing slope?
   Return true if yes
   """
   # We can refine this later, for now I am going to say:
-  # - 1) This value must be less than the next value
-  # - 2) The average of this value and the next 4 must be less than the
-  #   average of the following 5 values
+  # - 1) This pressure must be less than the next pressure
+  # - 2) The average of this pressure and the next 4 must be less than the
+  #   average of the following 5 pressures
   # - 3) from this point, the average of readings 0-2 < 1-3 < 2-4
-  if pos + 10 > len(values):
+  if pos + 10 > len(pressures):
     return False # No room for this kerfuffle
-  if values[pos] >= values[pos+1]: return False # condition 1
-  if np.mean(values[pos:pos+4]) >= np.mean(values[pos+5:pos+9]): return False
-  if np.mean(values[pos:pos+2]) >= np.mean(values[pos+1:pos+3]): return False
-  if np.mean(values[pos+1:pos+3]) >= np.mean(values[pos+2:pos+4]): return False
+  if pressures[pos] >= pressures[pos+1]: return False # condition 1
+  if np.mean(pressures[pos:pos+4]) >= np.mean(pressures[pos+5:pos+9]): return False
+  if np.mean(pressures[pos:pos+2]) >= np.mean(pressures[pos+1:pos+3]): return False
+  if np.mean(pressures[pos+1:pos+3]) >= np.mean(pressures[pos+2:pos+4]): return False
   return True
 
-def is_slope_end (values, pos):
+def is_slope_end (pressures, pos):
   """
-  Is this position in list of values the start of a plateau/downwards slope?
+  Is this position in list of pressures the start of a plateau/downwards slope?
   Return true if yes
   """
   # We can refine this later, for now I am going to say:
-  # - 1) This value is greater than or equal to the next
+  # - 1) This pressure is greater than or equal to the next
   # - 2) from this point, the average of readings 0-2 >= 1-3 >= 2-4
-  if pos + 5 > len(values):
+  if pos + 5 > len(pressures):
     return False # No room for this kerfuffle
-  if values[pos] < values[pos+1]: return False # condition 1
-  if (np.mean(values[pos:pos+2]) < np.mean(values[pos+1:pos+3]) and
-      np.mean(values[pos+1:pos+3]) < np.mean(values[pos+2:pos+4])): return False
+  if pressures[pos] < pressures[pos+1]: return False # condition 1
+  if (np.mean(pressures[pos:pos+2]) < np.mean(pressures[pos+1:pos+3]) and
+      np.mean(pressures[pos+1:pos+3]) < np.mean(pressures[pos+2:pos+4])): return False
   return True
 
-def calculate_slope(timestamps, values, start_pos, end_pos):
+def calculate_slope(timestamps, pressures, start_pos, end_pos):
   """
   Calculate the slope of a pressure (y axis) vs timestamp (x axis)
   pair of arrays, between specified start and end positions in the array
@@ -93,38 +93,38 @@ def calculate_slope(timestamps, values, start_pos, end_pos):
   x=[]
   for ts in timestamp_subset:
     x.append(ts.timestamp())
-  y = values[start_pos+1:end_pos+1]
-  slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+  y = pressures[start_pos+1:end_pos+1]
+  slope, intercept, r_pressure, p_pressure, std_err = stats.linregress(x,y)
   return slope * 60 # convert to bars per minute (from per second)
 
-def find_next_slope(timestamps, values, position):
+def find_next_slope(timestamps, pressures, position):
   """
   Find the start time, end time, and slope of the next rising slope after
   the specified position in the array of timestamps (x) vs pressures (y)
   """
   current_pos = position
-  list_length=len(values)
+  list_length=len(pressures)
   while current_pos < list_length:
-    if is_slope_start(values, current_pos):
-      new_pos = find_slope_end(values, current_pos)
+    if is_slope_start(pressures, current_pos):
+      new_pos = find_slope_end(pressures, current_pos)
       # current_pos is the entry BEFORE the rise
       # new_pos is the last entry in the rise
       # Calculate the slope between them
-      slope = calculate_slope(timestamps, values, current_pos, new_pos)
+      slope = calculate_slope(timestamps, pressures, current_pos, new_pos)
       return new_pos, timestamps[current_pos], timestamps[new_pos], slope
     else:
       current_pos +=1
-  return -999, timestamps[-1], timestamps[-1], 0 #Return value -999 means STOP
+  return -999, timestamps[-1], timestamps[-1], 0 #Return pressure -999 means STOP
  
 
-def find_slope_end(values, position):
+def find_slope_end(pressures, position):
   """
     Get the position of the end of the slope in progress
   """
   current_pos = position
-  list_length=len(values)
+  list_length=len(pressures)
   while current_pos < list_length:
-    if is_slope_end(values, current_pos):
+    if is_slope_end(pressures, current_pos):
       return current_pos
     else:
       current_pos +=1
@@ -137,14 +137,14 @@ def find_slopes(dict):
     timestamps (x) vs pressures (y)
   """
   slopes = []
-  # Sort the value/timestamp pairs by time
+  # Sort the pressure/timestamp pairs by time
   lists = sorted(dict.items())
-  timestamps, values = zip(*lists)
+  timestamps, pressures = zip(*lists)
   list_position = 0 # Start at the beginning of the list
 
   # Keep looking for slopes, if we don't find one, return -999 (so we don't just add 1 to it and end up back at the start!)
   while list_position >= 0:
-    list_position, start_time, end_time, slope  = find_next_slope(timestamps,values,list_position)
+    list_position, start_time, end_time, slope  = find_next_slope(timestamps,pressures,list_position)
     if list_position >=0:
       print (f'Ramp of {slope:.3} bar/min from {start_time.strftime("%d/%m %H:%M:%S")} to {end_time.strftime("%H:%M:%S")}')
 
@@ -154,7 +154,7 @@ def find_slopes(dict):
 
 
 # Main function
-# Read all CSV files to a dictionary of timestamp vs. value
+# Read all CSV files to a dictionary of timestamp vs. pressure
 # That should deduplicate, so the individual totals don't necessarily add up to the final total
 input_files=['data/Up_Pressure_Empty_1.txt',
     'data/Up_Pressure_Empty_2.txt',
